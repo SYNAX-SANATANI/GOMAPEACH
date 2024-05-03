e0f21fe8efc0816ebd739398ff9144a3c9857993
SYNAX-SANATANI <166273111+SYNAX-SANATANI@users.noreply.github.com>
2024-05-03 14:28:32 +0530

    Update repo.py

--- PROFESSOR/plugins/bot/repo.py
+++ PROFESSOR/plugins/bot/repo.py
@@ -18,25 +18,25 @@ start_txt = """
 @app.on_message(filters.command("repo"))
 async def start(_, msg):
     buttons = [
-        [ 
-          InlineKeyboardButton("ＡＤＤ ＭＥ ＢＡＢＹ", url=f"https://t.me/Professor_Sukoon_Bot?startgroup=True&admin=delete_messages+invite_users+pin_messages")
-        ],
         [
-          InlineKeyboardButton("ʜᴇʟᴘ", url="https://t.me/Friends_Chatting_Group_Friends_0"),
-          InlineKeyboardButton("ᴘʀᴏғᴇssᴏʀ", url="https://t.me/SOURABH_OWNER"),
+          InlineKeyboardButton("𝗚𝗼𝗺𝗮 𝗣𝗲𝗮𝗰𝗵 [ 1 ]", url="https://t.me/Friends_Chatting_Group_Friends_0"),
+          InlineKeyboardButton("𝗚𝗼𝗺𝗮 𝗣𝗲𝗮𝗰𝗵 [ 2 ]", url="https://t.me/SOURABH_OWNER"),
+          ],
+        [
+          InlineKeyboardButton("𝗚𝗼𝗺𝗮 𝗣𝗲𝗮𝗰𝗵 [ 3 ]", url="https://t.me/Friends_Chatting_Group_Friends_0"),
+          InlineKeyboardButton("𝗚𝗼𝗺𝗮 𝗣𝗲𝗮𝗰𝗵 [ 4 ]", url="https://t.me/SOURABH_OWNER"),
           ],
                [
-                InlineKeyboardButton("ᴘʀᴏғᴇssᴏʀ ɴᴇᴛᴡᴏʀᴋ", url="https://t.me/PROFESSOR_NETWORK"),
-
-],
+          InlineKeyboardButton("𝗚𝗼𝗺𝗮 𝗣𝗲𝗮𝗰𝗵 [ 5 ]", url="https://t.me/Friends_Chatting_Group_Friends_0"),
+          InlineKeyboardButton("𝗚𝗼𝗺𝗮 𝗣𝗲𝗮𝗰𝗵 [ 6 ]", url="https://t.me/SOURABH_OWNER"),
+          ],
 [
-              InlineKeyboardButton("ᴍᴀɪɴ ʙᴏᴛ", url=f"https://t.me/Professor_Sukoon_Bot"),
-              InlineKeyboardButton("︎ᴍʏ ʀᴇᴘᴏ ", url=f"https://github.com/PROFESSOR-SOURABH/PROFESSOR-MUSIC"),
-       
-    ],
+          InlineKeyboardButton("𝗚𝗼𝗺𝗮 𝗣𝗲𝗮𝗰𝗵 [ 7 ]", url="https://t.me/Friends_Chatting_Group_Friends_0"),
+          InlineKeyboardButton("𝗚𝗼𝗺𝗮 𝗣𝗲𝗮𝗰𝗵 [ 8 ]", url="https://t.me/SOURABH_OWNER"),
+          ],          
     [
-              InlineKeyboardButton("sᴛʀɪɴɢ ɢᴇɴ", url=f"https://t.me/String_Generate_op_bot"),
-              InlineKeyboardButton("︎sᴛʀɪɴɢ ʜᴀᴄᴋ", url=f"https://t.me/ProfessorStringHackBot"),
+              InlineKeyboardButton("𝗚𝗼𝗺𝗮 𝗣𝗲𝗮𝗰𝗵 [ 9 ]", url=f"https://t.me/String_Generate_op_bot"),
+              InlineKeyboardButton("︎𝗚𝗼𝗺𝗮 𝗣𝗲𝗮𝗰𝗵 [ 10 ]", url=f"https://t.me/ProfessorStringHackBot"),
        
     ]]
     
